0fb794acf45e4e94ce87c243aa8262d2da8b3264
alloboval <perfeo@tutanota.com>
2017-04-25 12:41:04 +0300

    Test commit new

--- EncryptionNET/EncryptionNET/EncryptionNET.cs
+++ EncryptionNET/EncryptionNET/EncryptionNET.cs
@@ -7,8 +7,17 @@ namespace EncryptionNET
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Permutation Encryption");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Existing string: Fuck_the_system");
+            Console.WriteLine("----------------------------------------");
+            Console.Write("Encrypted:        ");
             Console.WriteLine(PremutationEncryption.Encrypt(5, "Fuck_the_system"));
+            Console.WriteLine("----------------------------------------");
+            Console.Write("Decrypted with the same params:   ");
             Console.WriteLine(PremutationEncryption.Encrypt(-5, PremutationEncryption.Encrypt(5, "Fuck_the_system")));
+            Console.WriteLine("----------------------------------------");
             Console.ReadKey();
         }
     }
