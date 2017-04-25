e5e9b5d0da6eaeedd7c7c7f812ca06e90d58a710
alloboval <perfeo@tutanota.com>
2017-04-25 14:43:37 +0300

    New encrypt

--- EncryptionNET/EncryptionNET/EncryptionNET.cs
+++ EncryptionNET/EncryptionNET/EncryptionNET.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Premutation;
+using Substitution;
 
 namespace EncryptionNET
 {
@@ -18,6 +19,52 @@ namespace EncryptionNET
             Console.Write("Decrypted with the same params:   ");
             Console.WriteLine(PremutationEncryption.Encrypt(-5, PremutationEncryption.Encrypt(5, "Fuck_the_system")));
             Console.WriteLine("----------------------------------------");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Substitution Encryption");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Existing string: Fuck_the_system");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Existing alphabetA: ABCDEFGHIJKLMNOPQRSTUVWXYZ_");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Existing alphabetB: _ZYXWVUTSRQPONMLKJIHGFEDCBA");
+            Console.WriteLine("----------------------------------------");
+            SubstitutionEncryption.alphabetA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_";
+            SubstitutionEncryption.alphabetB = "_ZYXWVUTSRQPONMLKJIHGFEDCBA";
+            Console.Write("Encrypted:   ");
+            Console.WriteLine(SubstitutionEncryption.Encryption("Fuck_the_system"));
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Existing alphabetA: _ZYXWVUTSRQPONMLKJIHGFEDCBA");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Existing alphabetB: ABCDEFGHIJKLMNOPQRSTUVWXYZ_");
+            Console.WriteLine("----------------------------------------");
+            SubstitutionEncryption.alphabetB = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_";
+            SubstitutionEncryption.alphabetA = "_ZYXWVUTSRQPONMLKJIHGFEDCBA";
+            Console.Write("Decrypted with the same params:   ");
+            Console.WriteLine(SubstitutionEncryption.Encryption(SubstitutionEncryption.Encryption("Fuck_the_system")));
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Substitution+Permutation Encryption");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Existing string: Fuck_the_system");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Existing alphabetA: ABCDEFGHIJKLMNOPQRSTUVWXYZ_");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Existing alphabetB: _ZYXWVUTSRQPONMLKJIHGFEDCBA");
+            Console.WriteLine("----------------------------------------");
+            SubstitutionEncryption.alphabetA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_";
+            SubstitutionEncryption.alphabetB = "_ZYXWVUTSRQPONMLKJIHGFEDCBA";
+            Console.Write("Encrypted:   ");
+            string s = SubstitutionEncryption.Encryption("Fuck_the_system", 2);
+            Console.WriteLine(s);
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Existing alphabetA: _ZYXWVUTSRQPONMLKJIHGFEDCBA");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Existing alphabetB: ABCDEFGHIJKLMNOPQRSTUVWXYZ_");
+            Console.WriteLine("----------------------------------------");
+            SubstitutionEncryption.alphabetB = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_";
+            SubstitutionEncryption.alphabetA = "_ZYXWVUTSRQPONMLKJIHGFEDCBA";
+            Console.Write("Decrypted with the same params:   ");
+            Console.WriteLine(SubstitutionEncryption.Encryption(s, -2));
+
             Console.ReadKey();
         }
     }
---
+++ Substitution/Substitution.cs
@@ -0,0 +1,75 @@
+﻿namespace Substitution
+{
+    public class SubstitutionEncryption
+    {
+        public static string alphabetA { get; set; } // normal alphabet
+        public static string alphabetB { get; set; } // alphabet for replace
+
+        static SubstitutionEncryption()
+        {
+            if (alphabetA == null)
+            {
+                alphabetA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_";
+            }
+            if (alphabetB == null)
+            {
+                alphabetB = "_ZYXWVUTSRQPONMLKJIHGFEDCBA";
+            }
+        }
+
+        public static string Encryption(string sourceString)
+        {
+            string result = "";
+            sourceString = sourceString.ToUpper();
+            for(int i = 0; i < sourceString.Length; i++)
+            {
+                for(int j = 0; j < alphabetA.Length; j++)
+                {
+                    if (sourceString[i] == alphabetA[j])
+                    {
+                        result = result + alphabetB[j];
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static string Encryption(string sourceString, int shift)
+        {
+            string result = "";
+            sourceString = sourceString.ToUpper();
+            for (int i = 0; i < sourceString.Length; i++)
+            {
+                for (int j = 0; j < alphabetA.Length; j++)
+                {
+                    if (sourceString[i] == alphabetA[j])
+                    {
+                        result = result + alphabetB[getNewPosition(shift, j)];
+                    }
+                }
+            }
+            return result;
+        }
+
+
+        private static int getNewPosition(int shift, int CurrentPosition)
+        {
+            int newPosition;
+            if ((CurrentPosition + shift) >= alphabetB.Length)
+            {
+                newPosition = (CurrentPosition + shift) - alphabetB.Length;
+                return (0 + newPosition);
+            }
+            else if (CurrentPosition + shift < 0)
+            {
+                newPosition = alphabetB.Length - System.Math.Abs(CurrentPosition + shift);
+                return newPosition;
+            }
+            else
+            {
+                return (CurrentPosition + shift);
+            }
+
+        }
+    }
+}
